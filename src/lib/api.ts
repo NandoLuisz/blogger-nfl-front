5bb0c14216c363366734299baed803eebc27bdf9
Luis <luis.soares62@aluno.ifce.edu.br>
2025-08-15 16:23:08 -0300

    Configuração da URL 1.0

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "http://nfl-blogger-aws-dev.us-east-1.elasticbeanstalk.com",
     headers: {
         'Content-Type': 'application/json',
     },
